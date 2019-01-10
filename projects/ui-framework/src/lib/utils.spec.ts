fbae185156ed7383d816db21d1d71feaa8df2719
Ishai Borovoy <ishai.borovoy@hibob.com>
2019-01-10 12:00:30 +0200

    Fix description

--- projects/ui-framework/src/lib/utils.spec.ts
+++ projects/ui-framework/src/lib/utils.spec.ts
@@ -1,7 +1,7 @@
 import BUtils from './utils';
 
 describe('Utils', () => {
-  describe('replace', () => {
+  describe('Input Attributes', () => {
     it('Should add attributes to base input', () => {
       const inputTemplate = BUtils.addAttributesToBaseInput('[myCustomAttribute1]="1" [myCustomAttribute2]="2"');
       expect(inputTemplate).toContain('[myCustomAttribute1]="1" [myCustomAttribute2]="2">');
