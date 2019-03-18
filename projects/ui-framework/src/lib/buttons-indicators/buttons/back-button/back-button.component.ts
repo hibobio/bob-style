14b43a6b1acadb20b7f3310d855f2c8ba83c9e35
Pavel Golovin <pavelgolovin@Pavels-MacBook-Pro.local>
2019-03-18 10:22:57 +0200

    icon color uses enum, not string

--- projects/ui-framework/src/lib/buttons-indicators/buttons/back-button/back-button.component.ts
+++ projects/ui-framework/src/lib/buttons-indicators/buttons/back-button/back-button.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { ButtonSize, BackButtonType } from '../buttons.enum';
-import { Icons, IconSize } from '../../../icons/icons.enum';
+import { IconColor, Icons, IconSize } from '../../../icons/icons.enum';
 
 @Component({
   selector: 'b-back-button',
@@ -11,7 +11,7 @@ import { Icons, IconSize } from '../../../icons/icons.enum';
             [disabled]="disabled">
       <b-icon icon="${Icons.back_arrow_link}"
             size="${IconSize.small}"
-            [color]="disabled ? 'light' : 'dark'">
+            [color]="disabled ? iconColor.light : color">
       </b-icon>
       <ng-content></ng-content>
     </b-button>
@@ -25,7 +25,10 @@ export class BackButtonComponent {
   size: ButtonSize = ButtonSize.small;
   @Input() type?: BackButtonType = BackButtonType.secondary;
   @Input() disabled = false;
+  @Input() color: IconColor = IconColor.dark;
   @Output() clicked: EventEmitter<void> = new EventEmitter<void>();
+
+  iconColor = IconColor;
 
   onClick($event) {
     this.clicked.emit($event);
