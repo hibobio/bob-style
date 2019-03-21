89ee3a621ec8ad6c714a23efb289b85c681d3890
Pavel Golovin <pavelgolovin@Pavels-MacBook-Pro.local>
2019-03-21 11:42:30 +0200

    typo fixed in card test

--- projects/ui-framework/src/lib/cards/card/card.component.spec.ts
+++ projects/ui-framework/src/lib/cards/card/card.component.spec.ts
@@ -80,17 +80,17 @@ describe('CardComponent', () => {
   });
 
   describe('onMenuClose', () => {
-    it('should remove focusInside class from host element after timeout', fakeAsync(() => {
+    it('should remove focus-inside class from host element after timeout', fakeAsync(() => {
       component.menu = [{ label: '' }];
       fixture.detectChanges();
       const menuElement = fixture.debugElement.query(By.css('b-menu'));
       menuElement.componentInstance.openMenu.emit();
       menuElement.componentInstance.closeMenu.emit();
       fixture.detectChanges();
-      expect(fixture.nativeElement.classList).toContain('focusInside');
+      expect(fixture.nativeElement.classList).toContain('focus-inside');
       tick(300);
       fixture.detectChanges();
-      expect(fixture.nativeElement.classList).not.toContain('focusInside');
+      expect(fixture.nativeElement.classList).not.toContain('focus-inside');
     }));
   });
 });
