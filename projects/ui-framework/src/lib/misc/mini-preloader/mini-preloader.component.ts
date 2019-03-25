16741ba40bf8b9e868c73a67168289b97ce576df
Pavel Golovin <pavelgolovin@Pavels-MacBook-Pro.local>
2019-03-25 15:41:30 +0200

    new mini-preloader

--- projects/ui-framework/src/lib/misc/mini-preloader/mini-preloader.component.ts
+++ projects/ui-framework/src/lib/misc/mini-preloader/mini-preloader.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'b-mini-preloader',
-  template: `
-    <div class="fountainG fountainG_1"></div>
-    <div class="fountainG fountainG_2"></div>
-    <div class="fountainG fountainG_3"></div>
-  `,
-  styleUrls: ['./mini-preloader.component.scss'],
+  selector: 'b-mini-preloader, [b-mini-preloader]',
+  template: '',
+  styleUrls: ['./mini-preloader.component.scss']
 })
 export class MiniPreloaderComponent {
-
-  constructor() {
-  }
+  constructor() {}
 }
