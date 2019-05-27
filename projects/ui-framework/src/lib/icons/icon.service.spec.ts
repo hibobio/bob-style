a00c8929d08101d0c819f38c82f599038308382f
Ishai Borovoy <ishai.borovoy@hibob.com>
2019-05-27 16:54:47 +0300

    Fix test

--- projects/ui-framework/src/lib/icons/icon.service.spec.ts
+++ projects/ui-framework/src/lib/icons/icon.service.spec.ts
@@ -3,18 +3,20 @@ import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 import { TestBed } from '@angular/core/testing';
 import { Icons } from './icons.enum';
+import { Observable } from 'rxjs';
 
-describe('IconService', () => {
+fdescribe('IconService', () => {
   let iconService: IconService;
   let iconRegistry: jasmine.SpyObj<MatIconRegistry>;
   let sanitizer: jasmine.SpyObj<DomSanitizer>;
 
   beforeEach(() => {
-    const spyMatIconRegistry = jasmine.createSpyObj('MatIconRegistry', ['addSvgIcon']);
+    const spyMatIconRegistry = jasmine.createSpyObj('MatIconRegistry', ['addSvgIcon', 'getNamedSvgIcon']);
     const spyDomSanitizer =
       jasmine.createSpyObj('DomSanitizer', ['bypassSecurityTrustResourceUrl']);
     spyDomSanitizer.bypassSecurityTrustResourceUrl.and
       .callFake(val => val);
+    spyMatIconRegistry.getNamedSvgIcon.and.returnValue(new Observable());
     TestBed.configureTestingModule({
       providers: [
         IconService,
@@ -33,14 +35,11 @@ describe('IconService', () => {
       iconService.initIcon(Icons.toDos_link);
     });
     it('should call iconRegistry and sanitizer correct amount of times', () => {
-      expect(iconRegistry.addSvgIcon).toHaveBeenCalledTimes(1);
-      expect(sanitizer.bypassSecurityTrustResourceUrl).toHaveBeenCalledTimes(1);
+      expect(iconRegistry.getNamedSvgIcon).toHaveBeenCalledTimes(1);
     });
     it('should call iconRegistry and sanitizer with correct params', () => {
-      expect(iconRegistry.addSvgIcon.calls.first().args)
-        .toEqual(['todos_link', `https://images.hibob.com/icons/todos_link.svg`]);
-      expect(sanitizer.bypassSecurityTrustResourceUrl.calls.first().args)
-        .toEqual([`https://images.hibob.com/icons/todos_link.svg`]);
+      expect(iconRegistry.getNamedSvgIcon.calls.first().args)
+        .toEqual(['todos_link']);
     });
   });
 });
