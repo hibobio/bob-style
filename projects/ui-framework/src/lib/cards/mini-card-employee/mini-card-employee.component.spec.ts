865213a7d7b9fef1326d190cb747876c6e6aeda2
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-07-09 16:48:10 +0300

    - few tests.

--- projects/ui-framework/src/lib/cards/mini-card-employee/mini-card-employee.component.spec.ts
+++ projects/ui-framework/src/lib/cards/mini-card-employee/mini-card-employee.component.spec.ts
@@ -5,10 +5,12 @@ import { AvatarModule } from '../../buttons-indicators/avatar/avatar.module';
 import {TypographyModule} from '../../typography/typography.module';
 import {TruncateTooltipModule} from '../../services/truncate-tooltip/truncate-tooltip.module';
 import {mockAvatar} from '../../mock.const';
+import {By} from '@angular/platform-browser';
 
 describe('CardProfileComponent', () => {
   let component: MiniEmployeeCardComponent;
   let fixture: ComponentFixture<MiniEmployeeCardComponent>;
+  let mockComponentElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -31,5 +33,29 @@ describe('CardProfileComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('name should be colored', () => {
+    mockComponentElement = fixture.debugElement.query(
+      By.css('.name')
+    ).nativeElement;
+    expect(getComputedStyle(mockComponentElement).color).toEqual(
+      'rgb(83, 83, 83)'
+    );
+  });
+  it('title should be colored', () => {
+    mockComponentElement = fixture.debugElement.query(
+      By.css('.title')
+    ).nativeElement;
+    expect(getComputedStyle(mockComponentElement).color).toEqual(
+      'rgb(157, 157, 157)'
+    );
+  });
+  it('dates should be colored', () => {
+    mockComponentElement = fixture.debugElement.query(
+      By.css('.dates')
+    ).nativeElement;
+    expect(getComputedStyle(mockComponentElement).color).toEqual(
+      'rgb(48, 48, 48)'
+    );
   });
 });
