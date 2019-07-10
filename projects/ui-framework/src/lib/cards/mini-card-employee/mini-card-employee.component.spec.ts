d4f1ca3fc7a89991f73345f9f4309f47987cc228
Eliran Eliassy <eliran.eliassy@gmail.com>
2019-07-10 10:33:43 +0300

    - few more tests for card input.

--- projects/ui-framework/src/lib/cards/mini-card-employee/mini-card-employee.component.spec.ts
+++ projects/ui-framework/src/lib/cards/mini-card-employee/mini-card-employee.component.spec.ts
@@ -6,16 +6,18 @@ import {TypographyModule} from '../../typography/typography.module';
 import {TruncateTooltipModule} from '../../services/truncate-tooltip/truncate-tooltip.module';
 import {mockAvatar} from '../../mock.const';
 import {By} from '@angular/platform-browser';
+import {AvatarComponent} from '../../buttons-indicators/avatar/avatar.component';
 
 describe('CardProfileComponent', () => {
   let component: MiniEmployeeCardComponent;
   let fixture: ComponentFixture<MiniEmployeeCardComponent>;
   let mockComponentElement: HTMLElement;
-
+  let avatarComponent: AvatarComponent;
+  const mockAvatarSource = mockAvatar();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [AvatarModule, TypographyModule, TruncateTooltipModule],
-      declarations: [ MiniEmployeeCardComponent ],
+      declarations: [MiniEmployeeCardComponent],
     })
       .compileComponents()
       .then(() => {
@@ -24,7 +26,7 @@ describe('CardProfileComponent', () => {
         component.card = {
           name: 'Larry Murfiray',
           title: 'Product design',
-          imageSource: mockAvatar(),
+          imageSource: mockAvatarSource,
           dates: '11/07 - 20/07'
         };
         fixture.detectChanges();
@@ -34,6 +36,35 @@ describe('CardProfileComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have name rendered', () => {
+    mockComponentElement = fixture.debugElement.query(
+      By.css('.name')
+    ).nativeElement;
+    expect(mockComponentElement.innerText).toEqual('Larry Murfiray');
+  });
+
+  it('should have title rendered', () => {
+    mockComponentElement = fixture.debugElement.query(
+      By.css('.title')
+    ).nativeElement;
+    expect(mockComponentElement.innerText).toEqual('Product design');
+  });
+
+  it('should have dates rendered', () => {
+    mockComponentElement = fixture.debugElement.query(
+      By.css('.dates')
+    ).nativeElement;
+    expect(mockComponentElement.innerText).toEqual('11/07 - 20/07');
+  });
+
+  it('should have avatar image rendered', () => {
+    avatarComponent = fixture.debugElement.query(
+      By.css('b-avatar')
+    ).componentInstance;
+    expect(avatarComponent.imageSource).toEqual(mockAvatarSource);
+  });
+
   it('name should be colored', () => {
     mockComponentElement = fixture.debugElement.query(
       By.css('.name')
