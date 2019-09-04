ce75353ca1930b95bdd7ec3cd596aa9bbb212442
Ishai Borovoy <ishai.borovoy@hibob.com>
2019-09-04 11:47:32 +0300

    Make optional

--- projects/ui-framework/src/lib/table/table/table.interface.ts
+++ projects/ui-framework/src/lib/table/table/table.interface.ts
@@ -20,7 +20,7 @@ export interface ColumnDef {
   iconColor?: IconColor;
   cellClass?: (Icons | string)[];
   cellStyle?: any;
-  getQuickFilterText: Function;
+  getQuickFilterText?: Function;
 }
 
 export interface RowClickedEvent {
