673eeacf5c0c3b482bd55f3d91b8fa79bd344453
omermoshe <omerm80@gmail.com>
2019-12-26 10:58:04 +0200

    finshed the test for max chars update

--- projects/ui-framework/src/lib/lists/editable-list/editable-list.component.spec.ts
+++ projects/ui-framework/src/lib/lists/editable-list/editable-list.component.spec.ts
@@ -18,7 +18,7 @@ import { EditableListService } from './editable-list.service';
 import { editableListMock } from './editable-list.mock';
 import { getTestScheduler } from 'jasmine-marbles';
 
-  describe('EditableListComponent', () => {
+fdescribe('EditableListComponent', () => {
   let fixture: ComponentFixture<EditableListComponent>;
   let component: EditableListComponent;
   let selectOptionsMock: SelectOption[];
@@ -73,165 +73,166 @@ import { getTestScheduler } from 'jasmine-marbles';
       });
   }));
 
-    describe(' maxChars', () => {
-      it('should accept 10 chars if max chars is 10', () => {
-        component.maxChars = 10;
-        fixture.detectChanges();
-        const input = fixture.debugElement.query(By.css('.bel-item-input'));
-        expect(input.attributes.maxLength).toEqual('10');
+  describe(' maxChars', () => {
+    it('should accept 10 chars if max chars is 10', () => {
+      component.maxChars = 10;
+      fixture.detectChanges();
+      const input = fixture.debugElement.query(By.css('.bel-item-input'));
+      expect(input.attributes.maxLength).toEqual('10');
+    });
+    it('should get an error if the item on the list already', () => {
+      const input = fixture.debugElement.query(By.css('.bel-item-input'));
+      inputValue(input.nativeElement, component.list[0].value.toUpperCase());
+      fixture.detectChanges();
+      const done = fixture.debugElement.query(By.css('.bel-done-button'));
+      done.componentInstance.clicked.emit(null);
+      done.nativeElement.click();
+      fixture.detectChanges();
+      const error = fixture.debugElement.query(By.css('.b-input-message.error'));
+      expect(error.nativeElement.innerText).toContain(`"${selectOptionsMock[0].value}" already exists`);
+    });
+    it('check if has all items on the list', () => {
+      const list = fixture.debugElement.queryAll(By.css('.bel-item.b-icon-drag-alt'));
+      expect(list.length).toEqual(3);
+    });
+    it('check if an item list is deleted from the list', fakeAsync(() => {
+      const del = fixture.debugElement.query(By.css('.bel-trash-button button'));
+      del.nativeElement.click();
+      fixture.detectChanges();
+      const remove = fixture.debugElement.query(By.css('.bel-remove-button button'));
+      remove.nativeElement.click();
+      tick(300);
+      fixture.detectChanges();
+      const list3 = fixture.debugElement.queryAll(By.css('.bel-item.b-icon-drag-alt'));
+      expect(list3.length).toEqual(2);
+    }));
+    it('check if an item list is added to the list', () => {
+      const input = fixture.debugElement.query(By.css('.bel-item-input'));
+      inputValue(input.nativeElement, 'Drawing');
+      fixture.detectChanges();
+      const done = fixture.debugElement.query(By.css('.bel-done-button button'));
+      done.nativeElement.click();
+      fixture.detectChanges();
+      const list3 = fixture.debugElement.queryAll(By.css('.bel-item.b-icon-drag-alt'));
+      console.log(list3);
+      expect(list3.length).toEqual(4);
+    });
+    it('check if an item is deleted from the onListUpdate', fakeAsync(() => {
+      spyOn(component.changed, 'emit');
+      const del = fixture.debugElement.queryAll(By.css('.bel-trash-button button'));
+      del[2].nativeElement.click();
+      fixture.detectChanges();
+      const remove = fixture.debugElement.queryAll(By.css('.bel-remove-button button'));
+      remove[2].nativeElement.click();
+      tick(300);
+      fixture.detectChanges();
+      const expectedParam = {
+        'delete': [
+          'Football',
+        ],
+        'create': [],
+        'order': [
+          'Martial arts',
+          'Climbing',
+        ],
+        'sortType': 'UserDefined',
+        'list': [
+          {
+            'id': 1,
+            'value': 'Martial arts',
+            'selected': false,
+          },
+          {
+            'id': 2,
+            'value': 'Climbing',
+            'selected': false,
+          },
+        ],
+      };
+      expect(component.changed.emit).toHaveBeenCalledWith(expectedParam);
+    }));
+    it('check if an item is added to the onListUpdate', (done) => {
+      const input = fixture.debugElement.query(By.css('.bel-item-input'));
+      const doneButton = fixture.debugElement.query(By.css('.bel-done-button button'));
+      component.changed.subscribe((data) => {
+        expect(data.create[0]).toEqual('abc');
+        expect(data.order[0]).toEqual('abc');
+        done();
       });
-      it('should get an error if the item on the list already', () => {
-        const input = fixture.debugElement.query(By.css('.bel-item-input'));
-        inputValue(input.nativeElement, component.list[0].value.toUpperCase());
-        fixture.detectChanges();
-        const done = fixture.debugElement.query(By.css('.bel-done-button'));
-        done.componentInstance.clicked.emit(null);
-        done.nativeElement.click();
-        fixture.detectChanges();
-        const error = fixture.debugElement.query(By.css('.b-input-message.error'));
-        expect(error.nativeElement.innerText).toContain(`"${editableListMock[0].value}" already exists`);
-      });
-      it('check if has 10 items on the list', () => {
-        const list = fixture.debugElement.queryAll(By.css('.bel-item.b-icon-drag-alt'));
-        expect(list.length).toEqual(10);
-      });
-      it('check if an item list is deleted from the list', fakeAsync(() => {
-        const del = fixture.debugElement.query(By.css('.bel-trash-button button'));
-        del.nativeElement.click();
-        fixture.detectChanges();
-        const remove = fixture.debugElement.query(By.css('.bel-remove-button button'));
-        remove.nativeElement.click();
-        tick(300);
-        fixture.detectChanges();
-        const list3 = fixture.debugElement.queryAll(By.css('.bel-item.b-icon-drag-alt'));
-        expect(list3.length).toEqual(9);
-      }));
-      it('check if an item list is added to the list', () => {
-        const input = fixture.debugElement.query(By.css('.bel-item-input'));
-        inputValue(input.nativeElement, 'aa');
-        fixture.detectChanges();
-        const done = fixture.debugElement.query(By.css('.bel-done-button button'));
-        done.nativeElement.click();
-        fixture.detectChanges();
-        const list3 = fixture.debugElement.queryAll(By.css('.bel-item'));
-        expect(list3.length).toEqual(12);
-      });
-      it('check if an item is deleted from the onListUpdate', fakeAsync(() => {
-        spyOn(component.changed, 'emit');
-        const del = fixture.debugElement.queryAll(By.css('.bel-trash-button button'));
-        del[2].nativeElement.click();
-        fixture.detectChanges();
-        const remove = fixture.debugElement.queryAll(By.css('.bel-remove-button button'));
-        remove[2].nativeElement.click();
-        tick(300);
-        fixture.detectChanges();
-        const expectedParam = {
-          'delete': [
-            'Football'
-          ],
-          'create': [],
-          'order': [
-            'Martial arts',
-            'Climbing'
-          ],
-          'sortType': 'UserDefined',
-          'list': [
-            {
-              'id': 1,
-              'value': 'Martial arts',
-              'selected': false
-            },
-            {
-              'id': 2,
-              'value': 'Climbing',
-              'selected': false
-            }
-          ]
-        };
-        expect(component.changed.emit).toHaveBeenCalledWith(expectedParam);
-      }));
-      it('check if an item is added to the onListUpdate', (done) => {
-          const input = fixture.debugElement.query(By.css('.bel-item-input'));
-          const doneButton = fixture.debugElement.query(By.css('.bel-done-button button'));
-          component.changed.subscribe((data) => {
-          expect(data.create[0]).toEqual('abc');
-          expect(data.order[0]).toEqual('abc');
-          done();
-        });
-          inputValue(input.nativeElement, 'abc');
-          fixture.detectChanges();
-          doneButton.nativeElement.click();
-          fixture.detectChanges();
-      });
-      it('check the ASC button ', () => {
-        spyOn(component.changed, 'emit');
-        const sort = fixture.debugElement.query(By.css('.bel-sort-button button'));
-        sort.nativeElement.click();
-        fixture.detectChanges();
-        const expectedParam = {
-          'delete': [],
-          'create': [],
-          'order': [
-            'Climbing',
-            'Football',
-            'Martial arts',
-          ],
-          'sortType': 'Asc',
-          'list': [
-            {
-              'id': 2,
-              'value': 'Climbing',
-              'selected': false
-            },
-            {
-              'id': 3,
-              'value': 'Football',
-              'selected': true
-            },
-            {
-              'id': 1,
-              'value': 'Martial arts',
-              'selected': false
-            },
-          ]
-        };
-        expect(component.changed.emit).toHaveBeenCalledWith(expectedParam);
-      });
-      it('check the DSC button ', () => {
-        spyOn(component.changed, 'emit');
-        const sort = fixture.debugElement.query(By.css('.bel-sort-button button'));
-        sort.nativeElement.click();
-        sort.nativeElement.click();
-        fixture.detectChanges();
-        const expectedParam = {
-          'delete': [],
-          'create': [],
-          'order': [
-            'Martial arts',
-            'Football',
-            'Climbing',
-          ],
-          'sortType': 'Desc',
-          'list': [
-            {
-              'id': 1,
-              'value': 'Martial arts',
-              'selected': false
-            },
-            {
-              'id': 3,
-              'value': 'Football',
-              'selected': true
-            },
-            {
-              'id': 2,
-              'value': 'Climbing',
-              'selected': false
-            },
-          ]
-        };
-        expect(component.changed.emit).toHaveBeenCalledWith(expectedParam);
-      });
+      inputValue(input.nativeElement, 'abc');
+      fixture.detectChanges();
+      doneButton.nativeElement.click();
+      fixture.detectChanges();
+    });
+    it('check the ASC button ', () => {
+      spyOn(component.changed, 'emit');
+      const sort = fixture.debugElement.query(By.css('.bel-sort-button button'));
+      sort.nativeElement.click();
+      fixture.detectChanges();
+      const expectedParam = {
+        'delete': [],
+        'create': [],
+        'order': [
+          'Climbing',
+          'Football',
+          'Martial arts',
+        ],
+        'sortType': 'Asc',
+        'list': [
+          {
+            'id': 2,
+            'value': 'Climbing',
+            'selected': false,
+          },
+          {
+            'id': 3,
+            'value': 'Football',
+            'selected': true,
+          },
+          {
+            'id': 1,
+            'value': 'Martial arts',
+            'selected': false,
+          },
+        ],
+      };
+      expect(component.changed.emit).toHaveBeenCalledWith(expectedParam);
+    });
+    it('check the DSC button ', () => {
+      spyOn(component.changed, 'emit');
+      const sort = fixture.debugElement.query(By.css('.bel-sort-button button'));
+      sort.nativeElement.click();
+      sort.nativeElement.click();
+      fixture.detectChanges();
+      const expectedParam = {
+        'delete': [],
+        'create': [],
+        'order': [
+          'Martial arts',
+          'Football',
+          'Climbing',
+        ],
+        'sortType': 'Desc',
+        'list': [
+          {
+            'id': 1,
+            'value': 'Martial arts',
+            'selected': false,
+          },
+          {
+            'id': 3,
+            'value': 'Football',
+            'selected': true,
+          },
+          {
+            'id': 2,
+            'value': 'Climbing',
+            'selected': false,
+          },
+        ],
+      };
+      expect(component.changed.emit).toHaveBeenCalledWith(expectedParam);
+    });
   });
 });
 
