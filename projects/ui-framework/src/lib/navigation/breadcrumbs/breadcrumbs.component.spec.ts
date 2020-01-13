48a7bac51eefa60afabf92c5eb47faf085f12e12
Pasha Golovin <psh.glvn@gmail.com>
2020-01-13 19:44:44 +0200

    working on tests

--- projects/ui-framework/src/lib/navigation/breadcrumbs/breadcrumbs.component.spec.ts
+++ projects/ui-framework/src/lib/navigation/breadcrumbs/breadcrumbs.component.spec.ts
@@ -1,37 +1,40 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { By } from '@angular/platform-browser';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MobileService } from '../../services/utils/mobile.service';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
 import { Breadcrumb } from './breadcrumbs.interface';
-import { mobileServiceStub } from '../../tests/services.stub.spec';
+import { BreadcrumbsType, BreadcrumbsStepState } from './breadcrumbs.enum';
+import {
+  elementsFromFixture,
+  emitNativeEvent,
+} from '../../services/utils/test-helpers';
 
-describe('BreadcrumbsComponent', () => {
+fdescribe('BreadcrumbsComponent', () => {
   let component: BreadcrumbsComponent;
   let fixture: ComponentFixture<BreadcrumbsComponent>;
   let breadCrumbsMock: Breadcrumb[];
+  let stepsElements: HTMLElement[];
+  let titleElements: HTMLElement[];
 
   beforeEach(async(() => {
     breadCrumbsMock = [
-      { title: 'details' },
-      { title: 'avatar' },
-      { title: 'to dos' },
-      { title: 'summary' },
+      { title: 'details', state: BreadcrumbsStepState.success },
+      { title: 'avatar', state: BreadcrumbsStepState.active },
+      { title: 'to dos', state: BreadcrumbsStepState.closed },
+      { title: 'summary', state: BreadcrumbsStepState.closed },
     ];
 
     TestBed.configureTestingModule({
       declarations: [BreadcrumbsComponent],
-      imports: [NoopAnimationsModule, MatTooltipModule],
-      providers: [{ provide: MobileService, useValue: mobileServiceStub }],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [],
+      providers: [],
     })
       .compileComponents()
       .then(() => {
         fixture = TestBed.createComponent(BreadcrumbsComponent);
         component = fixture.componentInstance;
+        component.type = BreadcrumbsType.primary;
+        component.alwaysShowTitle = true;
         component.steps = breadCrumbsMock;
+        fixture.detectChanges();
 
         spyOn(component.stepClick, 'emit');
       });
@@ -39,55 +42,63 @@ describe('BreadcrumbsComponent', () => {
 
   describe('breadcrumbs model', () => {
     beforeEach(() => {
-      fixture.detectChanges();
+      stepsElements = elementsFromFixture(fixture, '.step');
+      titleElements = elementsFromFixture(fixture, '.step-title');
     });
+
     it('should generate 4 steps', () => {
-      const stepsElements = fixture.debugElement.queryAll(By.css('.step'));
       expect(stepsElements.length).toBe(4);
     });
-    it('should set disabled class on the last element', () => {
-      const stepsElements = fixture.debugElement.queryAll(By.css('.step'));
-      for (let i = 0; i < stepsElements.length; i++) {
-        if (i === 3) {
-          expect(stepsElements[i].nativeElement.classList).toContain(
-            'disabled'
-          );
-        } else {
-          expect(stepsElements[i].nativeElement.classList).not.toContain(
-            'disabled'
-          );
-        }
-      }
+
+    it('should set success state on first element', () => {
+      expect(stepsElements[0].getAttribute('data-state')).toEqual('success');
     });
-    it('should show step title on active state', () => {
-      const titleElement = fixture.debugElement.queryAll(By.css('.title'));
-      expect(titleElement.length).toEqual(1);
-      expect(titleElement[0].nativeElement.innerText).toEqual('to dos');
+
+    it('should set active state on second element', () => {
+      expect(stepsElements[1].getAttribute('data-state')).toEqual('active');
+    });
+
+    it('should show step titles', () => {
+      expect(titleElements[0].innerHTML).toContain('details');
+      expect(titleElements[2].innerHTML).toContain('to dos');
+    });
+
+    it('should make step with status success clickable', () => {
+      expect(titleElements[0].getAttribute('role')).toEqual('button');
+      expect(titleElements[0].getAttribute('tabindex')).toEqual('0');
+    });
+
+    it('should make stepы with status closed not clickable', () => {
+      expect(titleElements[2].getAttribute('role')).not.toEqual('button');
+      expect(titleElements[2].getAttribute('tabindex')).not.toEqual('0');
+    });
+
+    it('should set type attribute to primary', () => {
+      expect(
+        fixture.debugElement.nativeElement.getAttribute('data-type')
+      ).toEqual('primary');
+      expect(titleElements[2].getAttribute('tabindex')).not.toEqual('0');
+    });
+
+    it('should set data-always-show-title attribute', () => {
+      expect(
+        fixture.debugElement.nativeElement.getAttribute(
+          'data-always-show-title'
+        )
+      ).toEqual('false');
     });
   });
 
   describe('onStepClick', () => {
-    it('show emit value with index', () => {
-      fixture.detectChanges();
-      const step = fixture.debugElement.queryAll(By.css('.step'))[1];
-      step.triggerEventHandler('click', null);
-      fixture.detectChanges();
-      expect(component.stepClick.emit).toHaveBeenCalledWith(1);
+    it('should emit value with index, if step is clickable', () => {
+      const step = stepsElements[0].querySelector('.step-wrap');
+      emitNativeEvent(step);
+      expect(component.stepClick.emit).toHaveBeenCalledWith(0);
     });
-  });
-
-  describe('toggleStrategy', () => {
-    it('should display all titles', () => {
-      component.alwaysShowTitle = true;
-      fixture.detectChanges();
-      const titles = fixture.debugElement.queryAll(By.css('.title'));
-      expect(titles.length).toEqual(4);
-    });
-    it('should display only active title', () => {
-      component.alwaysShowTitle = false;
-      fixture.detectChanges();
-      const titles = fixture.debugElement.queryAll(By.css('.title'));
-      expect(titles.length).toEqual(1);
+    it('should not emit, if step is not clickable', () => {
+      const step = stepsElements[2].querySelector('.step-wrap');
+      emitNativeEvent(step);
+      expect(component.stepClick.emit).not.toHaveBeenCalled();
     });
   });
 });
