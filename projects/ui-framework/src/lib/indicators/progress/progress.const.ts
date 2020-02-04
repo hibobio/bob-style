65c5f56164313528bdb7ea4418155a922c11db07
Pasha Golovin <psh.glvn@gmail.com>
2020-02-04 10:06:55 +0200

    remove donut temporaly

--- projects/ui-framework/src/lib/indicators/progress/progress.const.ts
+++
@@ -1,7 +0,0 @@
-import { ProgressSize } from './progress.enum';
-
-export const ProgressDonutDiameter = {
-  [ProgressSize.small]: 32,
-  [ProgressSize.medium]: 46,
-  [ProgressSize.large]: 110,
-};
