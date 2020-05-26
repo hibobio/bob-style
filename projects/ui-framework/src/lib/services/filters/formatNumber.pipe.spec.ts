1d3f820c16f1cc521fe03d73894517b2d4748b62
Pasha Golovin <psh.glvn@gmail.com>
2020-05-26 15:50:26 +0300

    formatNumber pipe

---
+++ projects/ui-framework/src/lib/services/filters/formatNumber.pipe.spec.ts
@@ -0,0 +1,21 @@
+import { FormatNumberPipe } from './formatNumber.pipe';
+
+fdescribe('FormatNumberPipe', () => {
+  const pipe = new FormatNumberPipe();
+
+  it('should format number', () => {
+    expect(pipe.transform(123456.789)).toEqual('123,456.789');
+  });
+
+  it('should limit to 3 decimal points', () => {
+    expect(pipe.transform(123456.78987654321)).toEqual('123,456.79');
+  });
+
+  it('should format string that looks like number', () => {
+    expect(pipe.transform('123456.789')).toEqual('123,456.789');
+  });
+
+  it('should format NaN as 0', () => {
+    expect(pipe.transform('something')).toEqual('0');
+  });
+});
