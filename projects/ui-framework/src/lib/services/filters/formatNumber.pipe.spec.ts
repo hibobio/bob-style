4240a5a33bbe160999eb655526e31efea3c333ac
Pasha Golovin <pashagolovin@Pashas-Mac-mini.local>
2020-05-27 03:33:30 +0300

    same

--- projects/ui-framework/src/lib/services/filters/formatNumber.pipe.spec.ts
+++ projects/ui-framework/src/lib/services/filters/formatNumber.pipe.spec.ts
@@ -3,16 +3,12 @@ import { FormatNumberPipe } from './formatNumber.pipe';
 describe('FormatNumberPipe', () => {
   const pipe = new FormatNumberPipe();
 
-  it('should format number', () => {
-    expect(pipe.transform(123456.789)).toEqual('123,456.789');
-  });
-
-  it('should limit to 3 decimal points', () => {
-    expect(pipe.transform(123456.78987654321)).toEqual('123,456.79');
+  it('should format number and limit decimal points to 2', () => {
+    expect(pipe.transform(123456.789)).toEqual('123,456.79');
   });
 
   it('should format string that looks like number', () => {
-    expect(pipe.transform('123456.789')).toEqual('123,456.789');
+    expect(pipe.transform('123456.789')).toEqual('123,456.79');
   });
 
   it('should format NaN as 0', () => {
