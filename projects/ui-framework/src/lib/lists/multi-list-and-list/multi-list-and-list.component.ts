0f98bd490fb5e8283048dc01e441c22c6e41dfed
Pasha Golovin <pashagolovin@Pashas-MacBook-Pro.local>
2020-11-30 22:23:26 +0200

    MLAS maxHeight, misc

--- projects/ui-framework/src/lib/lists/multi-list-and-list/multi-list-and-list.component.ts
+++ projects/ui-framework/src/lib/lists/multi-list-and-list/multi-list-and-list.component.ts
@@ -1,6 +1,5 @@
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   ElementRef,
   EventEmitter,
@@ -24,6 +23,7 @@ import { ListChangeService } from '../list-change/list-change.service';
 import { MultiListAndSomething } from '../multi-list-and-chips/multi-list-and-something.interface';
 import { ListRow, ListViewConfig } from './multi-list-and-list.interface';
 import { MenuItem } from '../../navigation/menu/menu.interface';
+import { DOMhelpers } from '../../services/html/dom-helpers.service';
 
 @Component({
   selector: 'b-multi-list-and-list',
@@ -39,12 +39,12 @@ export class MultiListAndListComponent
   implements MultiListAndSomething<ListRow> {
   constructor(
     public host: ElementRef,
+    protected DOM: DOMhelpers,
     protected translate: TranslateService,
     protected listModelService: ListModelService,
-    protected listChangeService: ListChangeService,
-    private cd: ChangeDetectorRef
+    protected listChangeService: ListChangeService
   ) {
-    super(translate, listModelService, listChangeService);
+    super(host, DOM, translate, listModelService, listChangeService);
   }
 
   @ViewChild(BasicListComponent, { static: true })
