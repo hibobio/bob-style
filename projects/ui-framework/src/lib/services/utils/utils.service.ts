bf83652c322d20cb131f2855b2e149a76d0268fb
Pasha Golovin <pasha@galloween.net>
2020-12-21 01:43:42 +0200

    some more

--- projects/ui-framework/src/lib/services/utils/utils.service.ts
+++ projects/ui-framework/src/lib/services/utils/utils.service.ts
@@ -6,9 +6,9 @@ import { ScrollEvent, WinResizeEvent } from './utils.interface';
 import { insideZone } from './rxjs.operators';
 import { DocumentRef } from './document-ref.service';
 
-const appScrollContainerTokenFactory = function () {
+function appScrollContainerTokenFactory() {
   return '.app-content > .content-wrapper';
-};
+}
 
 export const APP_SCROLL_CONTAINER = new InjectionToken<string>(
   'App scrollable container selector',
