fbc0405c5f2e561d0742b69502e830bc7d3cae49
Pasha Golovin <pasha@galloween.net>
2021-04-01 23:16:48 +0300

    some more

--- projects/ui-framework/src/lib/services/utils/mutation-observable.ts
+++ projects/ui-framework/src/lib/services/utils/mutation-observable.ts
@@ -313,7 +313,8 @@ export class MutationObservableService {
               isIntersecting: isInView,
               isVisible: isInView,
             } as any) as IntersectionObserverableEntry)
-        )
+        ),
+        insideZone(this.zone)
       );
     }
 
