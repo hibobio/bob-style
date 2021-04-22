504ffffbec39889b860a54c937d7f8cd04fb51fe
itamar tal <itamar@e-square.io>
2021-04-22 14:24:40 +0300

    update info-strip style

--- projects/ui-framework/src/lib/indicators/info-strip/info-strip.component.ts
+++ projects/ui-framework/src/lib/indicators/info-strip/info-strip.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
 
 import { Icon } from '../../icons/icon.interface';
 import { Link } from '../link/link.types';
@@ -10,25 +10,14 @@ import { InfoStripIconSize, InfoStripIconType } from './info-strip.enum';
   templateUrl: './info-strip.component.html',
   styleUrls: ['./info-strip.component.scss'],
 })
-export class InfoStripComponent implements OnInit {
-  @Input() iconType: InfoStripIconType = InfoStripIconType.information;
+export class InfoStripComponent {
+  @HostBinding('attr.data-type') @Input() iconType: InfoStripIconType = InfoStripIconType.information;
   @Input() link: Link;
   @Input() text: string;
   @Input() iconSize: InfoStripIconSize = InfoStripIconSize.large;
   @Output() linkClicked: EventEmitter<void> = new EventEmitter<void>();
-  @HostBinding('class.warning') warning: boolean;
-  @HostBinding('class.information') information: boolean;
-  @HostBinding('class.error') error: boolean;
-  @HostBinding('class.success') success: boolean;
   readonly iconSizes = InfoStripIconSize;
   readonly iconsDic: Record<InfoStripIconType, Icon> = INFOSTRIP_ICON_DICT;
 
   constructor() {}
-
-  ngOnInit(): void {
-    this.warning = this.iconType === InfoStripIconType.warning;
-    this.information = this.iconType === InfoStripIconType.information;
-    this.error = this.iconType === InfoStripIconType.error;
-    this.success = this.iconType === InfoStripIconType.success;
-  }
 }
