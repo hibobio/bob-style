79fba120594bff850a0074018a5f9cc4c08601ec
Pasha Golovin <pasha@galloween.net>
2021-05-03 09:00:16 +0300

    sort stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
-import { setCompodocJson } from '@storybook/addon-docs/angular';
-import docJson from '../documentation.json';
 import '!style-loader!css-loader!sass-loader!../projects/ui-framework/src/lib/style/style.scss';
+
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+
+import docJson from '../documentation.json';
 
 setCompodocJson(docJson);
 export const parameters = {
@@ -8,6 +10,13 @@ export const parameters = {
   previewTabs: {
     'storybook/docs/panel': {
       hidden: true,
+    },
+  },
+  options: {
+    storySort: (a, b) => {
+      return a[1].id.localeCompare(b[1].id, undefined, {
+        numeric: true,
+      });
     },
   },
 };
