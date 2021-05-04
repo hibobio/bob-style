a9b36f0f280ea827240628b03641670464c47cd9
Pasha Golovin <pasha@galloween.net>
2021-05-04 16:04:20 +0300

    wip

--- projects/ui-framework/src/lib/templates/employee/ee-layout.stories.ts
+++ projects/ui-framework/src/lib/templates/employee/ee-layout.stories.ts
@@ -12,7 +12,11 @@ import { AvatarModule } from '../../avatar/avatar/avatar.module';
 import { ButtonsModule } from '../../buttons/buttons.module';
 import { CardsModule } from '../../cards/cards.module';
 import { ComponentGroupType } from '../../consts';
+import { EyeCandyModule } from '../../eye-candy/eye-candy.module';
 import { Icons } from '../../icons/icons.enum';
+import { ProgressBarModule } from '../../indicators/progress/progress-bar/progress-bar.module';
+import { ProgressDonutModule } from '../../indicators/progress/progress-donut/progress-donut.module';
+import { SwitchToggleModule } from '../../indicators/switch-toggle/switch-toggle.module';
 import {
   disease,
   funnyImg,
@@ -25,9 +29,15 @@ import { BreadcrumbsModule } from '../../navigation/breadcrumbs/breadcrumbs.modu
 import { MenuModule } from '../../navigation/menu/menu.module';
 import { SideMenuOption } from '../../navigation/side-menu/side-menu.interface';
 import { SideMenuModule } from '../../navigation/side-menu/side-menu.module';
+import { COLOR_PALETTE_SET1_COLOR_ORDER } from '../../services/color-service/color-palette.const';
+import {
+  ColorPalette,
+  ColorsGrey,
+} from '../../services/color-service/color-palette.enum';
 import {
   makeArray,
   randomFromArray,
+  randomNumber,
 } from '../../services/utils/functional-utils';
 import { StoryBookLayoutModule } from '../../story-book-layout/story-book-layout.module';
 import { TypographyModule } from '../../typography/typography.module';
@@ -38,65 +48,117 @@ const story = storiesOf(ComponentGroupType.Layout, module).addDecorator(
   withKnobs
 );
 
-const template = `<b-ee-layout
-  [type]="type"
-  [config]="config"
-  [avatar]="avatars[avIndx]">
+const template = `
+  <b-confetti #confetti
+              [colors]="confettiColors"
+              [numberOfConfetti]="30">
+  </b-confetti>
+  <b-snow #snow
+          [numberOfFlakes]="120"
+          [snowDuration]="10000">
+  </b-snow>
+<b-ee-layout
+      [type]="type"
+      [config]="config"
+      [avatar]="avatars[avIndx]">
 
-  <ng-container header>
+      <ng-container header>
 
-    <b-menu [menu]="headerMenu">
-      <b-chevron-button menu-trigger type="primary" text="Today's tasks">
-      </b-chevron-button>
-    </b-menu>
+        <b-menu [menu]="headerMenu">
+          <b-chevron-button menu-trigger type="primary" text="Today's tasks">
+          </b-chevron-button>
+        </b-menu>
 
-    <b-heading class="mrg-l-auto mrg-r-16">Your progress:</b-heading>
+        <b-heading class="mrg-l-auto mrg-r-16">Your progress:</b-heading>
 
-    <b-breadcrumbs type="primary"
-             [alwaysShowTitle]="true"
-             [steps]="breadcrumbs"></b-breadcrumbs>
+        <b-breadcrumbs type="primary"
+                [alwaysShowTitle]="true"
+                [steps]="breadcrumbs"></b-breadcrumbs>
 
-  </ng-container>
+      </ng-container>
 
-  <ng-container sidebar>
+      <ng-container sidebar>
 
-    <p class="b-quote mrg-t-24 mrg-b-0">
-      <span>{{joke}}</span>
-    </p>
+        <p class="b-quote mrg-t-24 mrg-b-0">
+          <span>{{joke}}</span>
+        </p>
 
-    <b-side-menu class="mrg-t-24" [options]="sideMenu"
-                headerLabel="Because">
-    </b-side-menu>
+        <b-side-menu class="mrg-t-24" [options]="sideMenu"
+                    headerLabel="Because">
+        </b-side-menu>
 
-  </ng-container>
+      </ng-container>
 
-  <ng-container title>
-    <b-display-2>You can be anyone you want to be</b-display-2>
-  </ng-container>
+      <ng-container title>
+        <b-display-2>Shiny happy people</b-display-2>
 
-  <ng-container content>
+        <b-group class="mrg-l-auto">
+          <b-square-button size="small" type="tertiary"
+                          [icon]="icons.file_upload"
+                          (clicked)="snow.ngOnDestroy();confetti.ngOnDestroy();confetti.fireConfetti(pos)">
+          </b-square-button>
+          <b-square-button size="small" type="tertiary"
+                          [icon]="icons.file_download"
+                          (clicked)="snow.ngOnDestroy();confetti.ngOnDestroy();snow.makeSnow()">
+          </b-square-button>
+        </b-group>
+      </ng-container>
 
-    <b-cards type="small">
-      <b-card-image *ngFor="let card of cards" [card]="card"
-                    type="small">
-      </b-card-image>
-    </b-cards>
+      <ng-container content>
 
-  </ng-container>
+        <div class="flx flx-row-align-y">
+          <ng-container *ngFor="let donut of donuts; let index=index; let last=last;">
+
+            <b-progress-donut *ngIf="index<3"
+              [data]="donut"
+              [size]="zoom ? 'medium' : 'small'"
+              [config]="{showValueInCenter:false}"
+              [class.brd-r]="!last"
+              [class.pad-r-16]="!last"
+              [class.mrg-r-16]="!last"></b-progress-donut>
+
+            <b-progress-bar *ngIf="index>=3"
+              class="mrg-l-auto"
+              type="primary"
+              [data]="donut"
+              [size]="zoom ? 'medium' : 'small'"
+              [config]="{hideValue:true}"></b-progress-bar>
+
+          </ng-container>
+        </div>
+
+        <div class="flx flx-row-align-y mrg-t-16">
+          <b-display-3>You can be anyone you want to be</b-display-3>
+
+          <b-switch-toggle
+            class="mrg-l-auto"
+            label="Make it larger"
+            [isChecked]="zoom" (changed)="zoom=$event"></b-switch-toggle>
+        </div>
+
+        <b-cards [type]="zoom ? 'regular' : 'small'">
+          <b-card-image *ngFor="let card of cards" [card]="card"
+                        [type]="zoom ? 'regular' : 'small'">
+          </b-card-image>
+        </b-cards>
+
+      </ng-container>
 
 </b-ee-layout>`;
 
 const note = `
- ## EE Layout
+  ## EE Layout
   #### Module
   *EELayoutModule*
+
   ~~~
   ${template}
   ~~~
+
   #### Properties
   Name | Type | Description | Default value
   --- | --- | --- | ---
-  [type] | EmployeeTemplateType | regular or transparentSidebar | regular
+  [type] | Types | style config preset | primary
 `;
 
 const storyTemplate = `
@@ -147,6 +209,9 @@ const storyTemplate = `
     bottom: -12px;
     left: -2px;*/
   }
+  b-progress-bar {
+    max-width: 250px;
+  }
 </style>
 <b-story-book-layout style="background-color:#faf7f2;" [title]="'EE Layout'">
     <div style="max-width:none;margin-top: -24px;">${template}</div>
@@ -196,6 +261,82 @@ const cards = funnyImg(9).map((img, i) => ({
   title: jobs[i + 10],
 }));
 
+const metrics1 = [
+  {
+    headerTextPrimary: 'Quality of work',
+    value: randomNumber(5, 50),
+  },
+  {
+    headerTextPrimary: 'Efficiency',
+    value: randomNumber(5, 50),
+  },
+  {
+    headerTextPrimary: 'Satisfaction',
+    value: randomNumber(5, 35),
+  },
+  {
+    headerTextPrimary: 'Health Index',
+    value: randomNumber(5, 35),
+  },
+  {
+    headerTextPrimary: 'Boredom',
+    value: randomNumber(50, 100),
+  },
+  {
+    headerTextPrimary: 'Happiness',
+    value: randomNumber(0, 50),
+  },
+  {
+    headerTextPrimary: 'Cultural fit',
+    value: randomNumber(50, 100),
+  },
+  {
+    headerTextPrimary: 'Absenteeism',
+    value: randomNumber(50, 100),
+  },
+];
+
+const metrics2 = [
+  {
+    headerTextPrimary: 'Goals achieved',
+    value: randomNumber(0, 30),
+  },
+  {
+    headerTextPrimary: 'Personal growth',
+    value: randomNumber(0, 30),
+  },
+];
+
+const donuts = randomFromArray(metrics1, 3)
+  .concat(randomFromArray(metrics2, 1))
+  .map((m, i) => {
+    const headerTextSecondary =
+      // i >= 3
+      //   ? null :
+      m.value < 50 / 3
+        ? 'none'
+        : m.value > 50 / 3 && m.value < (50 / 3) * 2
+        ? 'poor'
+        : m.value > (50 / 3) * 2 && m.value < 50
+        ? 'a little bit'
+        : m.value > 50 && m.value < 50 + 50 / 3
+        ? 'average'
+        : m.value > 50 + 50 / 3 && m.value < 50 + (50 / 3) * 2
+        ? 'considerable'
+        : 'overwhelming';
+    return {
+      ...m,
+      headerTextSecondary,
+      color: ColorPalette[COLOR_PALETTE_SET1_COLOR_ORDER[i]],
+      trackColor: ColorsGrey.color_grey_400,
+    };
+  });
+
+const pos: { x: number; y: number }[] = makeArray(5).map(() => ({
+  x: Math.random() * document.documentElement.clientWidth,
+  y: Math.random() * document.documentElement.clientHeight,
+}));
+
 story.add(
   'Employee template',
   () => {
@@ -205,14 +346,16 @@ story.add(
         type: select(
           'type',
           ['null', ...Object.keys(EE_LAYOUT_CONFIG_BY_TYPE)],
-          'primary'
+          'secondary'
         ),
 
         config: object('config', {
           headerClass: ['flx', 'flx-row-align-y', 'brd-b', 'pad-b-8'],
           contentClass: ['row-gap'],
+          sectionTitleClass: ['flx', 'flx-row-align-y'],
         }),
 
+        zoom: false,
         avIndx: 0,
         avatars: avatars,
         sideMenu: sideMenu,
@@ -237,6 +380,10 @@ story.add(
             state: 'closed',
           },
         ],
+        donuts: donuts,
+        pos: pos,
+        icons: Icons,
+        confettiColors: Object.values(ColorPalette),
       },
       moduleMetadata: {
         imports: [
@@ -250,6 +397,10 @@ story.add(
           BreadcrumbsModule,
           ButtonsModule,
           MenuModule,
+          ProgressDonutModule,
+          ProgressBarModule,
+          SwitchToggleModule,
+          EyeCandyModule,
         ],
         schemas: [CUSTOM_ELEMENTS_SCHEMA],
       },
