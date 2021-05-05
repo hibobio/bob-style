d1fc28de589d7b2ccbfc8865c4d2fa9addb90235
itamar tal <itamar@e-square.io>
2021-05-05 15:09:26 +0300

    remove ACTIONS_ICONS

--- projects/ui-framework/src/lib/lists/editable-list/editable-list.interface.ts
+++ projects/ui-framework/src/lib/lists/editable-list/editable-list.interface.ts
@@ -1,4 +1,4 @@
-import { IconActionsType, ListSortType } from './editable-list.enum';
+import { ListSortType } from './editable-list.enum';
 import { SelectOption } from '../list.interface';
 
 export interface EditableListActions {
@@ -16,6 +16,4 @@ export interface EditableListState {
   order: string[];
   list: SelectOption[];
 }
-
-export const ACTIONS_ICONS: Map<keyof EditableListActions, keyof typeof IconActionsType> = new Map([['edit', 'Rename'], ['remove', 'Delete']]) 
 
