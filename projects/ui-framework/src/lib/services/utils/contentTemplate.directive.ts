820418697c4557452a8c7410703890b216a8e5c3
Pasha Golovin <psh.glvn@gmail.com>
2021-05-24 14:40:41 +0300

    ContentTemplateConsumer - multiple templates support

--- projects/ui-framework/src/lib/services/utils/contentTemplate.directive.ts
+++ projects/ui-framework/src/lib/services/utils/contentTemplate.directive.ts
@@ -1,8 +1,11 @@
-import { ContentChild, Directive, NgModule, TemplateRef } from '@angular/core';
-
-export interface ContentTemplateConsumer {
-  contentTemplate: ContentTemplateDirective;
-}
+import {
+  ContentChildren,
+  Directive,
+  Input,
+  NgModule,
+  QueryList,
+  TemplateRef,
+} from '@angular/core';
 
 /**
  <ng-container *contentTemplate="let data=data">
@@ -12,6 +15,16 @@ export interface ContentTemplateConsumer {
   <ng-container *ngTemplateOutlet="contentTemplate?.tpl;
                 context: { data: item.data }">
   </ng-container>
+
+  // multiple templates:
+
+  <ng-container *contentTemplate="let data=data; name:'myTemplate'">
+    {{ data.title }}
+  </ng-container>
+
+    <ng-container *ngTemplateOutlet="getContentTemplate('myTemplate');
+                  context: { data: item.data }">
+    </ng-container>
  */
 
 @Directive({
@@ -20,14 +33,26 @@ export interface ContentTemplateConsumer {
 })
 export class ContentTemplateDirective {
   constructor(public tpl: TemplateRef<any>) {}
+  @Input() contentTemplateName: string;
 }
 
 @Directive()
 // tslint:disable-next-line: directive-class-suffix
-export abstract class ContentTemplateConsumer
-  implements ContentTemplateConsumer {
-  @ContentChild(ContentTemplateDirective, { static: true })
-  contentTemplate!: ContentTemplateDirective;
+export abstract class ContentTemplateConsumer {
+  @ContentChildren(ContentTemplateDirective)
+  private _cntntTmplts: QueryList<ContentTemplateDirective>;
+  public get contentTemplate() {
+    return this._cntntTmplts?.first || ({} as any);
+  }
+  public getContentTemplate(name: string) {
+    return (
+      (this._cntntTmplts?.length &&
+        this._cntntTmplts.find((dir) => {
+          return dir.contentTemplateName === name;
+        })) ||
+      this.contentTemplate
+    ).tpl;
+  }
 }
 
 @NgModule({
