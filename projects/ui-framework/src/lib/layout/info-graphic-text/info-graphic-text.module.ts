60def713ee7b2d9dcd9f3f5a1c97e5b7a9039034
Maxim Dicusari <maxim.dicusari@gmail.com>
2021-06-02 14:23:02 +0300

    - fix some PR comments

--- projects/ui-framework/src/lib/layout/info-graphic-text/info-graphic-text.module.ts
+++ projects/ui-framework/src/lib/layout/info-graphic-text/info-graphic-text.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 
 import { InfoGraphicTextComponent } from './info-graphic-text.component';
-import { TypographyModule } from '../../typography/typography.module';
 import { CommonModule } from '@angular/common';
 import { ButtonsModule } from '../../buttons/buttons.module';
 import { IconsModule } from '../../icons/icons.module';
@@ -9,7 +8,6 @@ import { IconsModule } from '../../icons/icons.module';
 
 @NgModule({
   imports: [
-    TypographyModule,
     CommonModule,
     ButtonsModule,
     IconsModule,
