4a6444b06ae2ee844b56d8046fe763e6526447fa
Maxim Dicusari <maxim.dicusari@gmail.com>
2021-06-02 15:04:05 +0300

    - fix the PR comments

--- projects/ui-framework/src/lib/layout/info-graphic-text/info-graphic-text.component.ts
+++ projects/ui-framework/src/lib/layout/info-graphic-text/info-graphic-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ButtonConfig, ButtonType, Icon, IconColor, Icons, IconSize } from 'bob-style';
 
 
@@ -7,25 +7,27 @@ import { ButtonConfig, ButtonType, Icon, IconColor, Icons, IconSize } from 'bob-
   templateUrl: './info-graphic-text.component.html',
   styleUrls: ['./info-graphic-text.scss']
 })
-export class InfoGraphicTextComponent implements OnInit {
+export class InfoGraphicTextComponent {
 
   @Input() items: string[];
   @Input() title: string;
-  @Input() buttonConfig: ButtonConfig = {
-    text: '',
-    type: ButtonType.negative,
-    onClick: () => console.log('button clicked !')
-  };
-  @Input() linkIconConfig: Icon = {
+  @Input() set buttonText(value: string) {
+    this.buttonConfig.text = value || '';
+  }
+
+  @Output() buttonClicked: EventEmitter<void> = new EventEmitter<void>();
+  readonly linkIconConfig: Icon = {
     size: IconSize.large,
     color: IconColor.negative,
     icon: Icons.chevron_right
+  };
+  readonly buttonConfig: ButtonConfig = {
+    text: '',
+    type: ButtonType.negative,
+    onClick: () => this.buttonClicked.emit(),
   };
 
   constructor() {
   }
 
-
-  ngOnInit() {
-  }
 }
