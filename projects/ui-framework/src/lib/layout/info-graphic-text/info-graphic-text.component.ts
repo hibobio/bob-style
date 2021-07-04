0cf9c0b1eeb68f39b8fe230403bfda38a29192d2
Miki Ashkenazi <miki.ashkenazi@hibob.io>
2021-07-04 17:33:37 +0300

    reset colors

--- projects/ui-framework/src/lib/layout/info-graphic-text/info-graphic-text.component.ts
+++ projects/ui-framework/src/lib/layout/info-graphic-text/info-graphic-text.component.ts
@@ -5,6 +5,7 @@ import { ButtonConfig } from '../../buttons/buttons.interface';
 import { ColorTextItem } from '../../eye-candy/text-colored-links/text-colored-links.interface';
 import { Icon } from '../../icons/icon.interface';
 import { IconColor, Icons, IconSize } from '../../icons/icons.enum';
+import { Color } from '../../types';
 
 @Component({
   selector: 'b-info-graphic-text',
@@ -12,20 +13,27 @@ import { IconColor, Icons, IconSize } from '../../icons/icons.enum';
   styleUrls: ['./info-graphic-text.scss'],
 })
 export class InfoGraphicTextComponent {
-  @Input() items: ColorTextItem[];
-  @Input() title: string;
-  @Input() buttonText: string;
-
-  @Output() buttonClicked: EventEmitter<void> = new EventEmitter();
-  readonly linkIconConfig: Icon = {
+  linkIconConfig: Icon = {
     size: IconSize.large,
     color: IconColor.negative,
     icon: Icons.chevron_right,
   };
-  readonly buttonConfig: ButtonConfig = {
+  buttonConfig: ButtonConfig = {
     type: ButtonType.negative,
     onClick: () => this.buttonClicked.emit(),
   };
+  useCustomColors = false;
+  
+  @Input() items: ColorTextItem[];
+  @Input() title: string;
+  @Input() buttonText: string;
+  @Input() set customColors(colors: {textColor: Color, bgColor: Color}) {
+    this.linkIconConfig.color = IconColor.custom;
+    this.buttonConfig.type = ButtonType.tertiary;
+    this.useCustomColors = true;
+  };
+
+  @Output() buttonClicked: EventEmitter<void> = new EventEmitter();
 
   constructor() {}
 }
