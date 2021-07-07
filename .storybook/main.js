c2fc89d2437875bd4d34037dc02c515382aec7fa
David <davidmail000@gmail.com>
2021-07-07 17:25:52 +0300

    clean

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,20 +10,12 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  // plugins: [
-  //   require('postcss-flexbugs-fixes'),
-  //   require('autoprefixer')({
-  //     flexbox: 'no-2009',
-  //   }),
-  //   new DisableWarnings()
-  // ],
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
 
     config.devServer = { stats: 'errors-only' };
-    // config.devServer.inline = false;
     // config.plugins.push(new DisableWarnings());
 
     // Return the altered config
