d8bd30efc286863bed6d1055ba699eff03e85746
David <davidmail000@gmail.com>
2021-07-07 17:18:11 +0300

    upgraded bob-style

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,15 +7,27 @@ module.exports = {
     '@storybook/addon-notes',
     '@storybook/addon-essentials',
   ],
+  core: {
+    builder: 'webpack5',
+  },
+  // plugins: [
+  //   require('postcss-flexbugs-fixes'),
+  //   require('autoprefixer')({
+  //     flexbox: 'no-2009',
+  //   }),
+  //   new DisableWarnings()
+  // ],
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
 
     config.devServer = { stats: 'errors-only' };
-    config.plugins.push(new DisableWarnings());
+    // config.devServer.inline = false;
+    // config.plugins.push(new DisableWarnings());
 
     // Return the altered config
     return config;
   },
 };
+
