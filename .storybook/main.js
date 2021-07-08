d78fc2f377c35a51fdb657125326f3ad19ddaf90
David <davidmail000@gmail.com>
2021-07-08 08:48:55 +0300

    wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,13 +10,13 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+  plugins:[new DisableWarnings()],
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
 
     config.devServer = { stats: 'errors-only' };
-    // config.plugins.push(new DisableWarnings());
 
     // Return the altered config
     return config;
